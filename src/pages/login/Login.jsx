20f4ea4c56ac8603770a8081d18899480580aa16
noob-ubaid <ubaidurrahman661@gmail.com>
2025-09-03 19:32:56 +0600

    added login page

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import login from "/login.png";
+import { Link } from "react-router";
+const Login = () => {
+  return (
+    <div className="w-full min-h-screen flex items-center justify-center">
+      <div className="flex flex-col lg:flex-row items-center justify-between gap-6 md:gap-16 lg:gap-24">
+        <div>
+          <img className="max-w-xl w-full mx-auto" src={login} alt="" />
+        </div>
+        <div className="px-4 md:px-0">
+          <div className=" dark:bg-white p-4 max-w-md border border-[#0F0F0F26] rounded-md">
+            <h2 className="text-2xl font-primary font-semibold mt-4 mb-2 border-b border-b-[#0F0F0F26] pb-4 text-center">
+              Login Your Account
+            </h2>
+            <form>
+              <label className="label text-[14px] font-secondary font-medium mb-1">
+                Email
+              </label>
+              <input
+                type="email"
+                className="input font-secondary border border-gray-300 rounded mt-1 p-1.5 outline-none w-full mb-2"
+                placeholder="Email"
+              />
+
+              <label className="label text-[14px] font-secondary font-medium mb-1">
+                Password
+              </label>
+              <input
+                type="password"
+                className="input font-secondary border border-gray-300 rounded mt-1 p-1.5 outline-none w-full mb-2"
+                placeholder="Password"
+              />
+
+              <div className="mt-2 flex justify-between">
+                <label className="flex items-center font-secondary gap-2">
+                  <input type="checkbox" />
+                  Remember me
+                </label>
+                <a className="link link-hover font-secondary">Forgot password?</a>
+              </div>
+
+              <button type="submit" className="bg-[#407BFF] text-white py-2 rounded font-primary font-medium mt-4 w-full">
+                Login
+              </button>
+
+              <p className="text-center text-[14px] font-secondary mt-2 font-medium">
+                Don't have an account?{" "}
+                <Link to="/register" className="text-red-400 underline">
+                  Register
+                </Link>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,8 +1,13 @@
 import { createBrowserRouter } from "react-router";
 import Home from "../pages/Home/Home";
+import Login from "../pages/login/Login";
 export const router = createBrowserRouter([
   {
     path: "/",
     Component : Home
+  },
+  {
+    path: "/login",
+    Component : Login
   },
 ]);
