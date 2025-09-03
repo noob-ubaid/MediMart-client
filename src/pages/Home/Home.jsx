946d6729a26d8919de29ae36f62d4a6346e89fec
noob-ubaid <ubaidurrahman661@gmail.com>
2025-09-03 19:00:40 +0600

    added navbar component

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NavbarDemo } from '../../components/Navbar/Navbar';
+
+const Home = () => {
+    return (
+        <div>
+            <NavbarDemo/>
+        </div>
+    );
+};
+
+export default Home;
