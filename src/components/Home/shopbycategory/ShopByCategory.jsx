a5dbc32ab17a84a2f95c23c2d5f55b4f90e7244f
noob-ubaid <ubaidurrahman661@gmail.com>
2025-09-12 19:18:18 +0600

    added hover card effect

--- src/components/Home/shopbycategory/ShopByCategory.jsx
+++ src/components/Home/shopbycategory/ShopByCategory.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
-import MainTitle from '../../../shared/MainTitle';
+import React from "react";
+import MainTitle from "../../../shared/MainTitle";
+import { useQuery } from "@tanstack/react-query";
+import { axiosSecure } from "../../../hooks/useAxiosSecure";
+import { HoverEffect } from "./AnimatedCard";
 const ShopByCategory = () => {
-
-    return (
-        <div>
-            <MainTitle text={`Shop By Category`}/>
-        </div>
-    );
+    const {data:projects,isPending} = useQuery({
+      queryKey : ['category'],
+      queryFn : async()=> {
+        const res = await axiosSecure.get('/category')
+        return res.data
+      }
+    })
+  return (
+    <div>
+      <MainTitle text={`Shop By Category`} />
+      <div>
+        <HoverEffect items={projects} />
+      </div>
+    </div>
+  );
 };
 
 export default ShopByCategory;
