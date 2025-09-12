745802306fb09bf4408ff011bfdfa5d1fad4a672
noob-ubaid <ubaidurrahman661@gmail.com>
2025-09-12 16:40:44 +0600

    added reuseable title

---
+++ src/components/Home/shopbycategory/ShopByCategory.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainTitle from '../../../shared/MainTitle';
+const ShopByCategory = () => {
+
+    return (
+        <div>
+            <MainTitle text={`Shop By Category`}/>
+        </div>
+    );
+};
+
+export default ShopByCategory;
---
+++ src/shared/MainTitle.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { motion } from "motion/react";
+const MainTitle = ({ text }) => {
+  return (
+    <motion.p
+      initial={{ y: 12, opacity: 0 }}
+      whileInView={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.7, stiffness: 300, type: "spring", delay: 0.4 }}
+      className="text-3xl md:text-4xl lg:text-5xl font-semibold text-white text-center font-primary my-12 md:my-14 lg:my-16"
+    >
+      {text}
+    </motion.p>
+  );
+};
+
+export default MainTitle;
