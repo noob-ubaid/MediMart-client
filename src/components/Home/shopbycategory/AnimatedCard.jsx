bf079e0e8f2be39ea88ed0332f3cc2a95417c4e4
noob-ubaid <ubaidurrahman661@gmail.com>
2025-09-12 20:44:42 +0600

    added show all and show less features

--- src/components/Home/shopbycategory/AnimatedCard.jsx
+++ src/components/Home/shopbycategory/AnimatedCard.jsx
@@ -1,10 +1,84 @@
+// import { cn } from "../../../lib/utils";
+// import { AnimatePresence, motion } from "motion/react";
+
+// import { useState } from "react";
+// import { Link } from "react-router";
+
+// export const HoverEffect = ({ items, className }) => {
+//   let [hoveredIndex, setHoveredIndex] = useState(null);
+
+//   return (
+//     <div
+//       className={cn(
+//         "grid grid-cols-1 md:grid-cols-2  lg:grid-cols-4 gap-1",
+//         className
+//       )}
+//     >
+//       {items?.map((item, idx) => (
+//         <motion.div
+//           initial={{ y: 15, opacity: 0 }}
+//           whileInView={{ y: 0, opacity: 1 }}
+//           viewport={{ once: true }}
+//           transition={{ duration: 0.75, delay: `0.${idx}` }}
+//           key={idx}
+//         >
+//           <Link
+//             to={`/dashboard`}
+//             className="relative group  block p-2 h-full w-full"
+//             onMouseEnter={() => setHoveredIndex(idx)}
+//             onMouseLeave={() => setHoveredIndex(null)}
+//           >
+//             <AnimatePresence>
+//               {hoveredIndex === idx && (
+//                 <motion.span
+//                   className="absolute inset-0 h-full w-full bg-slate-800/[0.8] block  rounded-3xl"
+//                   layoutId="hoverBackground"
+//                   initial={{ opacity: 0 }}
+//                   animate={{
+//                     opacity: 1,
+//                     transition: { duration: 0.15 },
+//                   }}
+//                   exit={{
+//                     opacity: 0,
+//                     transition: { duration: 0.15, delay: 0.2 },
+//                   }}
+//                 />
+//               )}
+//             </AnimatePresence>
+//             <Card>
+//               <h1 className="text-2xl font-semibold text-white font-primary">{item.title}</h1>
+//               <p className=" text-white/60 font-secondary mt-3">{item.description}</p>
+//             </Card>
+//           </Link>
+//         </motion.div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export const Card = ({ className, children }) => {
+//   return (
+//     <div
+//       className={cn(
+//         "rounded-2xl h-full w-full p-3 overflow-hidden bg-black border border-transparent dark:border-white/[0.2] group-hover:border-slate-700 relative z-20",
+//         className
+//       )}
+//     >
+//       <div className="relative z-50">
+//         <div className="p-4">{children}</div>
+//       </div>
+//     </div>
+//   );
+// };
+
 import { cn } from "../../../lib/utils";
 import { AnimatePresence, motion } from "motion/react";
 
 import { useState } from "react";
 import { Link } from "react-router";
 
-export const HoverEffect = ({ items, className }) => {
+export const HoverEffect = ({ items, className, show, setShow }) => {
+  const [data, setData] = useState(items);
   let [hoveredIndex, setHoveredIndex] = useState(null);
 
   return (
@@ -14,44 +88,97 @@ export const HoverEffect = ({ items, className }) => {
         className
       )}
     >
-      {items?.map((item, idx) => (
-        <motion.div
-          initial={{ y: 15, opacity: 0 }}
-          whileInView={{ y: 0, opacity: 1 }}
-          viewport={{ once: true }}
-          transition={{ duration: 0.75, delay: `0.${idx}` }}
-          key={idx}
-        >
-          <Link
-            to={`/dashboard`}
-            className="relative group  block p-2 h-full w-full"
-            onMouseEnter={() => setHoveredIndex(idx)}
-            onMouseLeave={() => setHoveredIndex(null)}
-          >
-            <AnimatePresence>
-              {hoveredIndex === idx && (
-                <motion.span
-                  className="absolute inset-0 h-full w-full bg-slate-800/[0.8] block  rounded-3xl"
-                  layoutId="hoverBackground"
-                  initial={{ opacity: 0 }}
-                  animate={{
-                    opacity: 1,
-                    transition: { duration: 0.15 },
-                  }}
-                  exit={{
-                    opacity: 0,
-                    transition: { duration: 0.15, delay: 0.2 },
-                  }}
-                />
-              )}
-            </AnimatePresence>
-            <Card>
-              <h1 className="text-2xl font-semibold text-white font-primary">{item.title}</h1>
-              <p className=" text-white/60 font-secondary mt-4">{item.description}</p>
-            </Card>
-          </Link>
-        </motion.div>
-      ))}
+      {!show ? (
+        <>
+          {data.slice(0, 6)?.map((item, idx) => (
+            <motion.div
+              initial={{ y: 15, opacity: 0 }}
+              whileInView={{ y: 0, opacity: 1 }}
+              viewport={{ once: true }}
+              transition={{ duration: 0.75, delay: `0.${idx}` }}
+              key={idx}
+            >
+              <Link
+                to={`/dashboard`}
+                className="relative group  block p-2 h-full w-full"
+                onMouseEnter={() => setHoveredIndex(idx)}
+                onMouseLeave={() => setHoveredIndex(null)}
+              >
+                <AnimatePresence>
+                  {hoveredIndex === idx && (
+                    <motion.span
+                      className="absolute inset-0 h-full w-full bg-slate-800/[0.8] block  rounded-3xl"
+                      layoutId="hoverBackground"
+                      initial={{ opacity: 0 }}
+                      animate={{
+                        opacity: 1,
+                        transition: { duration: 0.15 },
+                      }}
+                      exit={{
+                        opacity: 0,
+                        transition: { duration: 0.15, delay: 0.2 },
+                      }}
+                    />
+                  )}
+                </AnimatePresence>
+                <Card>
+                  <h1 className="text-2xl font-semibold text-white font-primary">
+                    {item.title}
+                  </h1>
+                  <p className=" text-white/60 font-secondary mt-3">
+                    {item.description}
+                  </p>
+                </Card>
+              </Link>
+            </motion.div>
+          ))}
+        </>
+      ) : (
+        <>
+          {data?.map((item, idx) => (
+            <motion.div
+              initial={{ y: 15, opacity: 0 }}
+              whileInView={{ y: 0, opacity: 1 }}
+              viewport={{ once: true }}
+              transition={{ duration: 0.75, delay: `0.${idx}` }}
+              key={idx}
+            >
+              <Link
+                to={`/dashboard`}
+                className="relative group  block p-2 h-full w-full"
+                onMouseEnter={() => setHoveredIndex(idx)}
+                onMouseLeave={() => setHoveredIndex(null)}
+              >
+                <AnimatePresence>
+                  {hoveredIndex === idx && (
+                    <motion.span
+                      className="absolute inset-0 h-full w-full bg-slate-800/[0.8] block  rounded-3xl"
+                      layoutId="hoverBackground"
+                      initial={{ opacity: 0 }}
+                      animate={{
+                        opacity: 1,
+                        transition: { duration: 0.15 },
+                      }}
+                      exit={{
+                        opacity: 0,
+                        transition: { duration: 0.15, delay: 0.2 },
+                      }}
+                    />
+                  )}
+                </AnimatePresence>
+                <Card>
+                  <h1 className="text-2xl font-semibold text-white font-primary">
+                    {item.title}
+                  </h1>
+                  <p className=" text-white/60 font-secondary mt-3">
+                    {item.description}
+                  </p>
+                </Card>
+              </Link>
+            </motion.div>
+          ))}
+        </>
+      )}
     </div>
   );
 };
@@ -60,7 +187,7 @@ export const Card = ({ className, children }) => {
   return (
     <div
       className={cn(
-        "rounded-2xl h-full w-full p-4 overflow-hidden bg-black border border-transparent dark:border-white/[0.2] group-hover:border-slate-700 relative z-20",
+        "rounded-2xl h-full w-full p-3 overflow-hidden bg-black border border-transparent dark:border-white/[0.2] group-hover:border-slate-700 relative z-20",
         className
       )}
     >
@@ -70,4 +197,3 @@ export const Card = ({ className, children }) => {
     </div>
   );
 };
-
--- src/components/Home/shopbycategory/ShopByCategory.jsx
+++ src/components/Home/shopbycategory/ShopByCategory.jsx
@@ -1,22 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import MainTitle from "../../../shared/MainTitle";
 import { useQuery } from "@tanstack/react-query";
 import { axiosSecure } from "../../../hooks/useAxiosSecure";
 import { HoverEffect } from "./AnimatedCard";
 const ShopByCategory = () => {
-    const {data:projects,isPending} = useQuery({
-      queryKey : ['category'],
-      queryFn : async()=> {
-        const res = await axiosSecure.get('/category')
-        return res.data
-      }
-    })
-    if(isPending) return <p>loading...</p>
+  const [show,setShow] = useState(false)
+  const { data: projects, isPending } = useQuery({
+    queryKey: ["category"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/category");
+      return res.data;
+    },
+  });
+  if (isPending) return <p>loading...</p>;
   return (
     <div>
       <MainTitle text={`Shop By Category`} />
       <div>
-        <HoverEffect items={projects} />
+        <HoverEffect show={show} setShow={setShow} items={projects} />
+        <div className="flex items-center justify-center mt-6">
+          {!show ? <button onClick={()=> setShow(true)} className="bg-white text-black px-8 py-2 rounded-full font-medium">
+            Show All
+          </button> : <button onClick={()=> setShow(false)} className="bg-white text-black px-8 py-2 rounded-full font-medium">
+            Show Less
+          </button>}
+        </div>
       </div>
     </div>
   );
